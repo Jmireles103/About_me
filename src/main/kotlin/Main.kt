13d5263523dfa0c1656fc660cee2e7b8c302a871
Jesus Mireles <jesusmireles43@ymail.com>
2023-01-18 19:36:15 -0600

    Initail Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    println("Hello my name is Jesus Mireles")
+    println("This is my third year here at MCC studying Mobile Design and Develpment")
+    println("I have a twin brother in this class with me")
+
+
+    println("Program arguments: ${args.joinToString()}")
+}
